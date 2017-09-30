4e3f7b1674f832d1a664e23956d4f03b82314876
livexia <istillalive@live.cn>
2017-09-30 17:06:46 +0800

    测试学习大部分urllib.request、urllib.error的内容

--- Urllib/2017-9_2th.py
+++ Urllib/2017-9_2th.py
@@ -1,27 +1,24 @@
 import urllib.request
 import urllib.error
 import urllib.parse
-import re
-import socket
-import socks
+import time
+
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 
-url = "https://steamspy.com"
-data = b''
-headers = { 'User-Agent' : user_agent }
+data = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0}).encode('utf-8')
+headers = {'User-Agent': user_agent }
 
-proxy_handler = urllib.request.ProxyHandler({'http': 'http://127.0.0.1:1080/'})
-# proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()
-# proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
-opener = urllib.request.build_opener(proxy_handler)
-opener.add_handler = [('User-Agent', user_agent)]
+proxy_handler = urllib.request.ProxyHandler({'http': 'http://127.0.0.1:1080/'})     #新建代理
+proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()     #设置基础认证
+proxy_auth_handler.add_password('realm', 'host', 'username', 'password')    #添加认证密钥
+opener = urllib.request.build_opener(proxy_handler)         #设置认证
+opener.addheaders = [('User-Agent', user_agent)]        #添加headers
 
 count = 0
 while 1:
-    #url = input("input the website url:")
     try:
-        Req = opener.open('https://www.google.com')
+        Req = opener.open('http://ident.me', data)
     except urllib.error.HTTPError as e:
         print("Error Code: ",e.code)
         count += 1
@@ -31,7 +28,7 @@ while 1:
     else:
         print(Req)
         break
-    if count == 5:
+    if count == 5:      #当错误计数达到五个时退出循环
         break
 
 
@@ -39,6 +36,8 @@ while 1:
 with open("result.txt",'wt') as f:
     urlinfo = Req.info()
     website = Req.read()
-    print(urlinfo,file=f)
-    print(website.decode('utf-8'),file=f)
+    print(urlinfo)
+    print(website)
+    print(urlinfo, file=f)
+    print(website,file=f)
 
