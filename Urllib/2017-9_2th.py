7fd0e978667b64508e41fc8cd56b97e9317724c6
livexia <istillalive@live.cn>
2017-09-30 20:50:46 +0800

    添加部分注释

--- Urllib/2017-9_2th.py
+++ Urllib/2017-9_2th.py
@@ -6,14 +6,14 @@ import time
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 
-data = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0}).encode('utf-8')
+data = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0}).encode('utf-8')   # 测试post方法的数据
 headers = {'User-Agent': user_agent }
 
-proxy_handler = urllib.request.ProxyHandler({'http': 'http://127.0.0.1:1080/'})     #新建代理
+proxy_handler = urllib.request.ProxyHandler({'http': 'http://127.0.0.1:1080/'})     # 新建代理
 proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()     #设置基础认证
-proxy_auth_handler.add_password('realm', 'host', 'username', 'password')    #添加认证密钥
-opener = urllib.request.build_opener(proxy_handler)         #设置认证
-opener.addheaders = [('User-Agent', user_agent)]        #添加headers
+proxy_auth_handler.add_password('realm', 'host', 'username', 'password')    # 添加认证密钥
+opener = urllib.request.build_opener(proxy_handler)         # 设置代理
+opener.addheaders = [('User-Agent', user_agent)]        # 添加headers
 
 count = 0
 while 1:
