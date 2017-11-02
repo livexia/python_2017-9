4cbc5c85ee7df35fcf620aba738476e870a4df89
livexia <istillalive@live.cn>
2017-11-02 13:17:37 +0800

    update proxy_pool.py

--- crawler/proxy_pool.py
+++ crawler/proxy_pool.py
@@ -5,25 +5,30 @@ import time
 import json
 
 def get_proxy():
-    return requests.get("http://127.0.0.1:32771/get/").content.decode()
+    return requests.get("http://115.159.124.234:32769/get/").content.decode()
 
 def delete_proxy(proxy):
-    requests.get("http://127.0.0.1:32771/delete/?proxy={}".format(proxy))
+    requests.get("http://115.159.124.234:32769/?proxy={}".format(proxy))
 
 def getHtml():
+    retry_count = 3
     proxy = get_proxy()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
     }
-    print("http://{}".format(proxy))
-    html = requests.get('http://httpbin.org/ip', proxies={"http": "http://{}".format(proxy)}, timeout=20, verify=False)
-    print('访问的代理地址为：' + html.content.decode())
-    return html
-    # delete_proxy(proxy)
-    # return None
+    while retry_count > 0:
+        try:
+            # 使用代理访问
+            html = requests.get('http://ident.me/', proxies={"http": "http://{}".format(proxy)}, headers = headers, timeout=20, verify=False)
+            return html
+        except Exception:
+            retry_count -= 1
+    # 出错5次, 删除代理池中代理
+    delete_proxy(proxy)
+    return None
 
 def get_proxy_pool_status():
-    return requests.get("http://127.0.0.1:32771/get_status/").content.decode('utf-8')
+    return requests.get("http://115.159.124.234:32769/get_status/").content.decode('utf-8')
 
 
 if __name__ == '__main__':
