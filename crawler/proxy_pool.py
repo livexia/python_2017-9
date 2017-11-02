e3d16d36dbec025a0c341cdabba0435796ef7502
livexia <istillalive@live.cn>
2017-11-02 13:42:46 +0800

    update proxy_pool.py

--- crawler/proxy_pool.py
+++ crawler/proxy_pool.py
@@ -37,5 +37,6 @@ if __name__ == '__main__':
     while 1:
         getHtml()
         time.sleep(10)
+        print("剩余可用IP数：" + str(json.loads(get_proxy_pool_status())))
         if '"useful_proxy": 0' in get_proxy_pool_status():
             break
